2ab9b7e2e5230171920f659c1ced3b907d30c39c
noureddine haddad <noureddinehaddad17@gmail.com>
2022-02-28 20:12:56 +0100

    ddds

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
-console.log("srart");
+console.log("start");
 const startTime = Date.now();
 window.onload = function () {
-   const endTime = Date.now();
-   document.getElementsByTagName("body")[0].classList.remove("loading-active");
-   document
-      .getElementsByClassName("loading__container")[0]
-      .classList.add("hidden");
+   const loadingTime = Date.now() - startTime;
+   setTimeout(() => {
+      document
+         .getElementsByTagName("body")[0]
+         .classList.remove("loading-active");
+      document
+         .getElementsByClassName("loading__container")[0]
+         .classList.add("hidden");
 
-   console.log("finish");
-   console.log(endTime - startTime);
+      console.log("finish at ", Date.now() - startTime);
+   }, 2000 - loadingTime);
 };
