57eb9c1b4295bef46c0ae6872de14391a58d85e4
noureddine haddad <noureddinehaddad17@gmail.com>
2022-02-28 19:49:03 +0100

    aze

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 console.log("srart");
+const startTime = Date.now();
 window.onload = function () {
-   document.getElementsByTagName("body")[0].classList.remove("disable");
+   const endTime = Date.now();
+   document.getElementsByTagName("body")[0].classList.remove("loading-active");
+   document
+      .getElementsByClassName("loading__container")[0]
+      .classList.add("hidden");
 
    console.log("finish");
+   console.log(endTime - startTime);
 };
