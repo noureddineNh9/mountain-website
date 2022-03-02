1d38dd60adb78ec3e34ece28f54cf3ba0df8e110
noureddine haddad <noureddinehaddad17@gmail.com>
2022-03-03 00:19:28 +0100

    responsive navbar

--- js/main.js
+++ js/main.js
@@ -13,3 +13,25 @@ window.onload = function () {
       console.log("finish at ", Date.now() - startTime);
    }, 2000 - loadingTime);
 };
+
+//--------------------- Navbar ----------------------
+window.addEventListener("scroll", function () {
+   if (this.scrollY !== 0) {
+      document.getElementById("navbar").classList.add("scroll__down");
+   } else {
+      document.getElementById("navbar").classList.remove("scroll__down");
+   }
+});
+
+window.addEventListener("resize", function () {
+   if (window.innerWidth > 900) {
+      this.document.getElementById("sidebar-checkbox").checked = false;
+   }
+});
+
+document.querySelectorAll("nav a").forEach((link) => {
+   link.addEventListener("click", () => {
+      this.document.getElementById("sidebar-checkbox").checked = false;
+   });
+});
+//------------------------------------------------------
